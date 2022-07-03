39a0f90bb00d3b6f8d2525205b7e375f46d4116f
terryjung0244 <terryjung0244@gmail.com>
2022-07-03 09:26:11 -0400

    control updated

---
+++ 3.control/4.switch.js
@@ -0,0 +1,19 @@
+//
+// switch
+// if else if else if else...if else
+
+console.clear();
+
+let date = 'Sundayr'
+
+switch (date) {
+  case 'Sunday':
+    console.log('Today is Sunday')
+    break;
+  case 'Friday':
+    console.log("123")
+    break;
+  default: 
+    console.log(`Go home ${date}`)
+    break;
+}
