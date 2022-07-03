d3db0c20b912e2cef03b7b55bdc5b539b14f56e4
terryjung0244 <terryjung0244@gmail.com>
2022-07-02 23:02:51 -0400

    switch updated

---
+++ 1.if.js
@@ -0,0 +1,5 @@
+// 조건문 Conditional Statement
+// if(조건) { }
+// if(조건) { } else { }
+// if(조건) { } else if(조건)
+
---
+++ 3.quiz.js
@@ -0,0 +1,13 @@
+// QUIZ! 
+
+let num = 3;
+
+if (num % 2 === 0) {
+  console.log('Thumbs Up') //even
+} else {
+  console.log('Thumbs Down') //odd
+}
+
+let num2 = num % 2 === 0 ? 'Thumbs Up' : 'Thumbs Down';
+console.log(num2);
+
